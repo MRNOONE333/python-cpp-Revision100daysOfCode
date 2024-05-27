e9ba1a1335873df84f88194d2e2545718de61443
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-05-28 05:26:05 +0530

    day 15

---
+++ cpp_revision/7ternary_operator_else_if.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x =11;
+
+string result = (x<12)? "less than 12" : "greater";
+cout << result;
+return 0 ;
+}
+
---
+++ cpp_revision/8while_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<< "gg";
+    int i =0;
+    while (i<=5){
+        cout<< i ;
+        i++;
+    }
+}
